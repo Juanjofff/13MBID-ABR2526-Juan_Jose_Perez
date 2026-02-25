91a9aa1561fa9e5e03347993534ea4d577c83345
Juanjofff <juanjo_p_j@hotmail.es>
2026-02-24 22:08:42 -0500

    Se genera el script para aplicar las transformaciones y se genera el dataset a usar ne la siguiente etapa. #9 #10 #13

---
+++ src/data_preparation.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+
+INPUT_CSV = "data/raw/bank-additional-full.csv"
+OUTPUT_CSV = "data/processed/bank-processed.csv"
+
+def preprocess_data(input_csv: str = INPUT_CSV, output_csv: str = OUTPUT_CSV):
+    """ Prepara los datos para el modelo
+    
+    """
+    # Leer los datos
+    df = pd.read_csv(input_csv, sep=';')
+
+    # Adaptar nombres de columnas 
+    df.columns = df.columns.str.replace(".", "_")
+
+    # transformar los valores 'unknown' en NaN
+    df.replace('unknown', np.nan, inplace=True)
+
+    # se elimina la columna 'default'
+    df.drop(columns=['default'], inplace=True)
+
+    # se hace un filtro para eliminar las filas con valores NaN
+    df.dropna(inplace=True)
+
+    # se hace un filtro para eliminar las filas duplicadas
+    df.drop_duplicates(inplace=True)
+
+    # save the processed dataset
+    df.to_csv(output_csv, index=False)
+
+    return df.shape
+
+if __name__ == "__main__":
+    rows, columns = preprocess_data()
+    with open('docs/transformation.txt', 'w') as f:
+        f.write("Transformaciones realizadas:")
+        f.write("- Se reemplazaron los valores 'unknown' por NaN \n ")
+        f.write("- Se eliminaron las filas con valores nulos \n ")
+        f.write("- Se eliminaron las filas duplicadas \n ")
+        f.write("- Se eliminaron la columna 'default' debido a la cantidad de valores desconocidos (nulos) \n ")
+        f.write(f"- Cantidad de filas finales: {rows}")
+        f.write(f"- Cantidad de columnas finales: {columns}")
